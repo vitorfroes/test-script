84766c06d2d6e2860fc2b12a3f20a42c349795fd
Vitor Froes <vitorfroes@hotmail.com>
2021-05-18 15:35:54 -0300

    removed Bot script

--- src/pages/Client/index.js
+++ src/pages/Client/index.js
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
 
 const Client = () => {
-  useEffect(() => {
-    const div = document.getElementById("root");
-    buildBot(
-      div,
-      document,
-      "https://cdn-bot.qa.hiplatform.com/dtbot.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b",
-      "&widget=true&tab=true&text=Alguma%20d%C3%BAvida%3F&textcolor=ffffff&bgcolor=3d70bd&from=bottomright&widgetType=circle&iconId=",
-      "dtbot-script"
-    );
-  }, []);
+  // useEffect(() => {
+  //   const div = document.getElementById("root");
+  //   buildBot(
+  //     div,
+  //     document,
+  //     "https://cdn-bot.qa.hiplatform.com/dtbot.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b",
+  //     "&widget=true&tab=true&text=Alguma%20d%C3%BAvida%3F&textcolor=ffffff&bgcolor=3d70bd&from=bottomright&widgetType=circle&iconId=",
+  //     "dtbot-script"
+  //   );
+  // }, []);
 
-  const buildBot = (h, i, b, o, t) => {
-    if (window.HiBot !== undefined) return true;
-    var s = i.createElement("script");
-    s.src = b + o;
-    s.async = true;
-    s.id = t;
-    h.appendChild(s);
-  };
+  // const buildBot = (h, i, b, o, t) => {
+  //   if (window.HiBot !== undefined) return true;
+  //   var s = i.createElement("script");
+  //   s.src = b + o;
+  //   s.async = true;
+  //   s.id = t;
+  //   h.appendChild(s);
+  // };
 
   return (
     <>
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,19 +1,22 @@
+import { useEffect } from "react";
+
 const User = () => {
+  useEffect(() => {
+    const faq = document.getElementById("faq");
+    const script = document.createElement("script");
+    script.id = "dt-faq-script";
+    script.src =
+      "http://faq.qa.directtalk.com.br/1.0/static/dist/dt-faq.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b&appearance=0ee58c04-7217-4b2d-849e-a5e31c4c83a4";
+    script.async = true;
+
+    faq.appendChild(script);
+  }, []);
   return (
     <>
       <header className="App-header">
         <h1>User</h1>
       </header>
-      <p>Teste</p>
-      <p>Teste</p>
-      <p>Teste</p>
-      <p>Teste</p>
-      <p>Teste</p>
-
-      <script
-        id="dt-faq-script"
-        src="http://faq.qa.directtalk.com.br/1.0/static/dist/dt-faq.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b&appearance=0ee58c04-7217-4b2d-849e-a5e31c4c83a4"
-      ></script>
+      <div id="faq"></div>
     </>
   );
 };
