bde686ad1b463a42741da6adc2a2a71b644fee0a
Vitor Froes <vitorfroes@hotmail.com>
2021-05-18 16:02:24 -0300

    fixed url

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -6,7 +6,7 @@ const User = () => {
     const script = document.createElement("script");
     script.id = "dt-faq-script";
     script.src =
-      "http://faq.qa.directtalk.com.br/1.0/static/dist/dt-faq.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b&appearance=0ee58c04-7217-4b2d-849e-a5e31c4c83a4";
+      "https://faq.qa.directtalk.com.br/1.0/static/dist/dt-faq.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b&appearance=0ee58c04-7217-4b2d-849e-a5e31c4c83a4";
     script.async = true;
 
     faq.appendChild(script);
