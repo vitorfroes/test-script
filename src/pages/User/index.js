91eeb23a182e8c284897d5008327b7daeeda0869
Vitor Froes <vitorfroes@hotmail.com>
2021-05-18 14:53:10 -0300

    added FAQ

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,26 +1,4 @@
-import { useEffect } from "react";
-
 const User = () => {
-  useEffect(() => {
-    const div = document.getElementById("root");
-    buildBot(
-      div,
-      document,
-      "https://cdn-bot.qa.hiplatform.com/dtbot.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b",
-      "&widget=true&tab=true&text=Alguma%20d%C3%BAvida%3F&textcolor=ffffff&bgcolor=3d70bd&from=right&widgetType=circle&iconId=",
-      "dtbot-script"
-    );
-  }, []);
-
-  const buildBot = (h, i, b, o, t) => {
-    if (window.HiBot !== undefined) return true;
-    var s = i.createElement("script");
-    s.src = b + o;
-    s.async = true;
-    s.id = t;
-    h.appendChild(s);
-  };
-
   return (
     <>
       <header className="App-header">
@@ -31,6 +9,11 @@ const User = () => {
       <p>Teste</p>
       <p>Teste</p>
       <p>Teste</p>
+
+      <script
+        id="dt-faq-script"
+        src="http://faq.qa.directtalk.com.br/1.0/static/dist/dt-faq.js?token=7f7bec33-1f55-4ced-8f1b-7c204f35e12b&appearance=0ee58c04-7217-4b2d-849e-a5e31c4c83a4"
+      ></script>
     </>
   );
 };
