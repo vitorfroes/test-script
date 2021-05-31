7ef7e38ea29cc4785d9adf12ff4771c65cd12b69
Vitor Froes <vitorfroes@hotmail.com>
2021-05-31 14:29:01 -0300

    rollback

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -6,7 +6,7 @@ const User = () => {
     const script = document.createElement("script");
     script.id = "dt-faq-script";
     script.src =
-      "https://faq.directtalk.com.br/1.0/static/dist/dt-faq.js?token=886c96de-95c4-44a4-b54f-9bed4c9c33a6&appearance=6175a339-096b-11e7-94c7-1231004265b1";
+      "https://faq.directtalk.com.br/1.0/static/dist/dt-faq.js?token=671715a3-f067-493d-a2d0-3c16d9c145e4&appearance=dcb3f402-f7c3-4ada-9511-74347e4594bb";
     script.async = true;
 
     faq.appendChild(script);
