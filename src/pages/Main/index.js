9a0247762aaea1ccddd499ae15c117b4a11e5d75
Vitor Fróes <vitor.souza@hiplatform.com>
2021-11-10 17:23:32 -0300

    added reset on page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -28,6 +28,32 @@ const Main = () => {
     );
   }
 
+  function handleReset() {
+    const requestUrl = `https://www.facebook.com/v12.0/dialog/oauth?
+    client_id=1076671459078829
+    &redirect_uri="https://kind-lichterman-2d9143.netlify.app/"
+    &auth_type=rerequest
+    &scope=email,user_messenger_contact
+    &messenger_page_id=100918475750023
+    &reset_messenger_state=1`;
+
+    const headers = new Headers();
+    const initFetch = {
+      method: "GET",
+      headers: headers,
+      mode: "no-cors",
+      cache: "default",
+    };
+
+    fetch(requestUrl, initFetch)
+      .then((response) => {
+        console.log({ response });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -38,14 +64,29 @@ const Main = () => {
         muitoam interesanteam.
         <div>
           {!logged ? (
-            <button title="Login Facebook" onClick={handleLogin}>
+            <button
+              title="Login Facebook"
+              className="login-btn"
+              onClick={handleLogin}
+            >
               Login Facebook
             </button>
           ) : (
-            <button title="Logout" onClick={handleLogout}>
+            <button title="Logout" className="login-btn" onClick={handleLogout}>
               Logout
             </button>
           )}
+        </div>
+        <div className="reset">
+          Reset FB Login Connected
+          <button
+            type="button"
+            title="reset"
+            className="reset-btn"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
         </div>
       </main>
       <footer>
