ee7bea6f03ba836f19220144a6595ffbb67b60bb
Vitor Fróes <vitor.souza@hiplatform.com>
2021-11-10 17:44:11 -0300

    added FB Api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -29,29 +29,42 @@ const Main = () => {
   }
 
   function handleReset() {
-    const requestUrl = `https://www.facebook.com/v12.0/dialog/oauth?
-    client_id=1076671459078829
+    // const requestUrl = `https://www.facebook.com/v12.0/dialog/oauth?
+    // client_id=1076671459078829
+    // &redirect_uri="https://kind-lichterman-2d9143.netlify.app/"
+    // &auth_type=rerequest
+    // &scope=email,user_messenger_contact
+    // &messenger_page_id=100918475750023
+    // &reset_messenger_state=1`;
+
+    // const headers = new Headers();
+    // const initFetch = {
+    //   method: "GET",
+    //   headers: headers,
+    //   mode: "no-cors",
+    //   cache: "default",
+    // };
+
+    // fetch(requestUrl, initFetch)
+    //   .then((response) => {
+    //     console.log({ response });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    window.FB.api(
+      `/oauth?
+    client_id=${1076671459078829}
     &redirect_uri="https://kind-lichterman-2d9143.netlify.app/"
-    &auth_type=rerequest
+    &auth_type=rerequest&scope=email,user_messenger_contact
     &scope=email,user_messenger_contact
-    &messenger_page_id=100918475750023
-    &reset_messenger_state=1`;
-
-    const headers = new Headers();
-    const initFetch = {
-      method: "GET",
-      headers: headers,
-      mode: "no-cors",
-      cache: "default",
-    };
-
-    fetch(requestUrl, initFetch)
-      .then((response) => {
+    &messenger_page_id=${100918475750023}
+    &reset_messenger_state=1`,
+      (response) => {
         console.log({ response });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      }
+    );
   }
 
   return (
