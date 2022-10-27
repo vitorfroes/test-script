7778290878d06c8e4254203e1b8d73d003c8d197
Vitor Fróes <vitor.souza@hiplatform.com>
2022-10-27 13:46:49 -0300

    removed all bot and faq scripts

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,20 +3,20 @@ import { useEffect, useRef } from "react";
 const Main = () => {
   const divRef = useRef(null);
 
-  useEffect(() => {
-    if (divRef) {
-      createScript(divRef.current);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (divRef) {
+  //     createScript(divRef.current);
+  //   }
+  // }, []);
 
-  const createScript = (elem) => {
-    const script = document.createElement("script");
-    script.id = "dt-faq-script";
-    script.src =
-      "https://faq.directtalk.com.br/1.0/static/dist/v2/dt-faq.js?token=1e237c79-654f-4352-854a-d88ca3292936&appearance=a2187b4d-88b5-42ab-95d6-15ccd116eef9";
+  // const createScript = (elem) => {
+  //   const script = document.createElement("script");
+  //   script.id = "dt-faq-script";
+  //   script.src =
+  //     "https://faq.directtalk.com.br/1.0/static/dist/v2/dt-faq.js?token=1e237c79-654f-4352-854a-d88ca3292936&appearance=a2187b4d-88b5-42ab-95d6-15ccd116eef9";
 
-    elem.appendChild(script);
-  };
+  //   elem.appendChild(script);
+  // };
 
   return (
     <div className="App">
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 
 const User = () => {
-  useEffect(() => {
-    const faq = document.getElementById("faq");
-    const script = document.createElement("script");
-    script.id = "dt-faq-script";
-    script.src =
-      "https://faq.directtalk.com.br/1.0/static/dist/dt-faq.js?token=671715a3-f067-493d-a2d0-3c16d9c145e4&appearance=dcb3f402-f7c3-4ada-9511-74347e4594bb";
-    script.async = true;
+  // useEffect(() => {
+  //   const faq = document.getElementById("faq");
+  //   const script = document.createElement("script");
+  //   script.id = "dt-faq-script";
+  //   script.src =
+  //     "https://faq.directtalk.com.br/1.0/static/dist/dt-faq.js?token=671715a3-f067-493d-a2d0-3c16d9c145e4&appearance=dcb3f402-f7c3-4ada-9511-74347e4594bb";
+  //   script.async = true;
 
-    faq.appendChild(script);
-  }, []);
+  //   faq.appendChild(script);
+  // }, []);
   return (
     <>
       <header className="App-header">
