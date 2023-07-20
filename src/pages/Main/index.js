bd7c85d02d708df916f5c2ea304a56b3aea90d1f
Vitor Fróes <vitor.souza@hiplatform.com>
2023-07-20 16:32:58 -0300

    changed faq script

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,20 +3,20 @@ import { useEffect, useRef } from "react";
 const Main = () => {
   const divRef = useRef(null);
 
-  // useEffect(() => {
-  //   if (divRef) {
-  //     createScript(divRef.current);
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (divRef) {
+      createScript(divRef.current);
+    }
+  }, []);
 
-  // const createScript = (elem) => {
-  //   const script = document.createElement("script");
-  //   script.id = "dt-faq-script";
-  //   script.src =
-  //     "https://faq.directtalk.com.br/1.0/static/dist/v2/dt-faq.js?token=1e237c79-654f-4352-854a-d88ca3292936&appearance=a2187b4d-88b5-42ab-95d6-15ccd116eef9";
+  const createScript = (elem) => {
+    const script = document.createElement("script");
+    script.id = "dt-faq-script";
+    script.src =
+      "https://faq.qa.directtalk.com.br/1.0/static/dist/dt-faq.js?token=47aba87a-84a9-4151-a84a-813603ca475c&appearance=51481a6f-4b8a-4205-ba8a-74f4049526e1&goTop=false&action=search&value=0fa34845-6229-424c-8339-5f81d27fdc15";
 
-  //   elem.appendChild(script);
-  // };
+    elem.appendChild(script);
+  };
 
   return (
     <div className="App">
